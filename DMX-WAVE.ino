7d2d56633ae5f8fa96e48b4098b72235bee99025
engelphi <philipp.engel.1990@googlemail.com>
2023-05-11 18:27:26 +0200

    add some more programs

--- DMX-WAVE.ino
+++ DMX-WAVE.ino
@@ -162,15 +162,79 @@ void dmx_wave4() {
   delay(500);
 }
 
+void dmx_wave5() {
+  for (int i = 0; i < 4; i++) {
+    dmx_write(255, 0, 255, 0);
+    delay(500);
+    dmx_write(0, 255, 0, 255);
+    delay(500);
+  }
+  dmx_write(0, 0, 0, 0);
+  delay(500);
+}
+
+void dmx_wave6() {
+  for (int i = 0; i < 10; i++) {
+    long channel = random(4);
+    switch (channel) {
+      case 0: dmx_write(255, 0, 0, 0); break;
+      case 1: dmx_write(0, 255, 0, 0); break;
+      case 2: dmx_write(0, 0, 255, 0); break;
+      case 3: dmx_write(0, 0, 0, 255); break;
+    }
+    delay(500);
+  }
+  dmx_write(0, 0, 0, 0);
+  delay(500);
+}
+
+void dmx_wave7() {
+  dmx_write(255, 0, 0, 0);
+  delay(250);
+  dmx_write(0, 255, 0, 0);
+  delay(250);
+  dmx_write(255, 255, 0, 0);
+  delay(250);
+  dmx_write(0, 0, 255, 0);
+  delay(250);
+  dmx_write(255, 0, 255, 0);
+  delay(250);
+  dmx_write(0, 255, 255, 0);
+  delay(250);
+  dmx_write(255, 255, 255, 0);
+  delay(250);
+  dmx_write(0, 0, 0, 255);
+  delay(250);
+  dmx_write(255, 0, 0, 255);
+  delay(250);
+  dmx_write(0, 255, 0, 255);
+  delay(250);
+  dmx_write(255, 255, 0, 255);
+  delay(250);
+  dmx_write(0, 0, 255, 255);
+  delay(250);
+  dmx_write(255, 0, 255, 255);
+  delay(250);
+  dmx_write(0, 255, 255, 255);
+  delay(250);
+  dmx_write(255, 255, 255, 0);
+  delay(250);
+  dmx_write(0, 0, 0, 0);
+  delay(250);
+}
+
 void loop() {
   // button_state = digitalRead(Button);
   // if (button_state == LOW) {
-  long program = random(4);
+  long program = random(7);
   switch (program) {
     case 0: dmx_wave1(); break;
     case 1: dmx_wave2(); break;
     case 2: dmx_wave3(); break;
     case 3: dmx_wave4(); break;
+    case 4: dmx_wave5(); break;
+    case 5: dmx_wave6(); break;
+    case 6: dmx_wave7(); break;
   }
   // }
 }
